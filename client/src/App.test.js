4fcec238cfdc16a1e3d8a695f094ed28699260b0
Mike Harley <mharley12345@gmail.com>
2019-09-06 11:05:08 -0700

    styled

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,6 @@ import { render} from '@testing-library/react'
 test('loads', async ()=>{
   const { getByText } = render(<App/>)
   getByText('Players')
-
+  getByText('dark')
   getByText((content, element) => content.startsWith('Player'))
 })
