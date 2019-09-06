499aac0cf507d7156ac2704a13d0a00f2fe98ff1
Mike Harley <mharley12345@gmail.com>
2019-09-06 10:42:26 -0700

    ?MVP

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { render} from '@testing-library/react'
+test('loads', async ()=>{
+  const { getByText } = render(<App/>)
+  getByText('Players')
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  getByText((content, element) => content.startsWith('Player'))
+})
